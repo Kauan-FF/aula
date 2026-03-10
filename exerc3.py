4e63c1a9ddb337b1be8014d7e866a0ed6081db55
unknown <20241pvai0030017@estudantes.ifpr.edu.br>
2026-03-10 09:22:05 -0300

    Exerc 3

---
+++ exerc3.py
@@ -0,0 +1,10 @@
+x = float(input("Digite o 1 valor"))
+y = float(input("Digite o 2 valor"))
+
+def soma_valores(x,y):
+    resp = x + y
+    return resp
+
+valor = soma_valores(x,y)
+print(f"a Resposta da soma de {x} + {y} + {valor}" )
+
