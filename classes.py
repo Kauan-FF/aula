b47226cfb03a3dbfa50681fd4d2c0593d46e2f2d
Kauan Santos <20241pvai0030017@estudantes.ifpr.edu.br>
2026-04-17 16:40:11 -0300

    refazer instrução

--- classes.py
+++ classes.py
@@ -1,7 +1,46 @@
 class Jogador:
-    pass
+    def __init__ (self,nome_jg, nick,ape):
+        self.nome_jogador = nome_jg
+        self.nick = nick
+        self.apelido = ape
+
+
+    def cadastro_jogadores (self):
+        print(f"Jogador {self.nome_jogador}: adicionado")
+
+    def consultar_jogadores (self,lista):
+        for estudante in lista:
+            print(f"Jogador: {estudante.nome_jogador}/ nickname: {estudante.nick}/ Apelido {estudante.apelido}")
+
+    def procurar_nick (self):
+        print(f"Nick: {self.nome_jogador} / {self.nick} / {self.apelido}")
+
 class Equipe:
-    pass
+    def __init__ (self,nome_eq,jg):
+        self.nome_equipe = nome_eq
+        self.jogo = jg 
+        self.jogadores = []
+
+
+    def cadastro_equipe (self):
+        print(f"Equipe: {self.nome_equipe}: adicionado")
+
+
+    def adicionar_jogador(self):
+        print(f"Jogador: {self.jogador} adicionado")
+
+
+    def consultar_membros (self):
+        if(len(self.jogadores) >= 1):
+            print(f"Nome da equipe: {self.nome_equipe}")
+            for i in self.jogadores:
+                print(f"Jogador: {i.jogadores}")
+        else:
+            print("Nenhuma equipe cadastrada")
+
+    def consultar_equipes(self):
+                print(f"Nome: {self.nome_equipe} ")
+        
 
     
     
--- main.py
+++ main.py
@@ -1,2 +1,73 @@
 from classes import Jogador
 from classes import Equipe
+
+equipes = []
+jogadores = []
+
+while True:
+    print("\n=== MENU ===")
+    print("1 - Cadastrar Jogadores")
+    print("2 - Consultar jogadores")
+    print("3 - Cadastrar equipe")
+    print("4 - Consultar nomes das equipe")
+    print("5 - Consultar jogadores da equipe")
+    print("6 - Buscar jogador por nickname")
+    print("7 - Sair")
+
+    opcao = int(input("Escolha uma opção: "))
+
+    if opcao == 1:
+        nome_jg = input("Digite o nome do jogador: ")
+        nick = input("Digite o nick do jogador: ")
+        ape = input("Digite o apelido do jogador: ")
+
+        jogador = Jogador(nome_jg, nick, ape)
+        jogadores.append(jogador)
+        jogador.cadastro_jogadores()
+    
+    elif opcao == 2:
+        nome_eq = input("Digite o nome da equipe: ")
+        jg = input("Digite o nome do jogo da equipe: ")
+        equipe= Equipe(nome_eq,jg)
+        equipes.append(equipe)
+        equipe.cadastro_equipe()
+
+    elif opcao == 3:
+        equi = input("Digite o nome da equipe")
+        for eq in equipes:
+            if(equi == eq.nome_equipe):
+                nome = input("Digite o nome do jogador")
+                for j in jogadores:
+                    if(nome == j.nome_jogador):
+                        eq.jogadores.append(j)
+                        equipe.adicionar_jogador()
+
+    elif opcao == 4:
+        if(len(equipes) >= 1):
+            for equipe in equipes:
+                equipe.consultar_equipes()
+        else:
+            print("Nenhuma equipe cadastrada")
+
+    elif opcao == 5:
+        nome = input("Digite o nome da equipe")
+        for x in equipes:
+            if( nome == x.nome_equipe): 
+                x.consultar_membros()
+
+
+    elif opcao == 6:
+        busca = input("Digite o nick do jogador")
+        if(len(jogadores) == 0):
+            print("Nenhum jogador cadastrado")
+            for k in jogadores:
+                if(busca == k.nick ):
+                    k.procurar_nick()
+
+                else:
+                    print("Jogador não encontrado")
+    elif opcao == 0:
+        print("Saindo....")
+
+    else:
+        print("Digite um numero de 1 a 6")
